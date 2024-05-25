cfdfdcc2b7c68a41c259787b311a7689f03c21b8
Rishabh Shukla <rishabh.489.c@gmail.com>
2024-05-25 10:15:27 +0530

    done zod

--- index1.js
+++ index1.js
@@ -1,14 +1,26 @@
 const express =require('express');
+const z=require("zod"); 
 
 const app=express();
-
+const schema=z.array(z.number())
 app.use(express.json());
 
 app.post("/health-checkup",(req,res)=>{
     const kidneys =req.body.kidneys;
-    const kidneylength=kidneys.length;
-    res.send("length of kidney is "+kidneylength);
+   const response =schema.safeParse(kidneys);
+   if(!response.success){
+    res.status(411).json({
+        msg: "input is invalid"
+    })
+   }else{res.send({response});}
+    
 
 });
+
+app.use((err,req,res,next)=>{
+    res.json({
+        msg: "something went wrong"
+    })
+})
 
 app.listen(3000);
